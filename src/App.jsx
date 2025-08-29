12d0d6dac8f50a1a1731d1a1778aaebcbf8b8abb
S0NSHIN3 <jimmyalejandrobc@ufps.edu.co>
2025-08-29 17:18:36 -0500

    creacion section y contador

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header'
+import {Header} from './components/Header'
+import {Section} from './components/Section'
 
 function App() {
 
@@ -10,9 +11,13 @@ function App() {
 
   return (
 <>
-<Header show ={true}>
+<Header show ={true}>git 
   <p>Header</p>
 </Header>
+<Section>
+  <p>Section</p>
+</Section>
+
 </>
   )
 }
---
+++ src/components/Section.jsx
@@ -0,0 +1,47 @@
+import { use, useState } from "react"
+
+const users =[{
+    "id": 1,
+    "nombre": "Juan",
+    "email": "juan@gmail.com",
+    "imagen": null,
+    "perfil": "admin"
+},
+{
+    "id": 2,
+    "nombre": "Ana",
+    "email": "hola@gmail.com",
+    "imagen": null,
+    "perfil": "user"
+    }
+]
+export const Section = () => {
+    const [cont,setCont] = useState(0)
+    const handleClick = () => {
+        setCont(cont + 1)
+        console.log("evento de contador") + cont
+    }
+  return (
+    <>
+    <h2>Contador #{cont} {cont%2 == 0 ? "par":"impar"}</h2>
+    <button onClick={handleClick}>Aumentar</button>
+    <ul>
+        {
+
+            users.map(user => <li key = {user.id}>{user.nombre}</li>)
+                    
+        }
+
+    </ul>
+    <ul>
+        {
+    
+    users.map({user,index} =() => {return <li key = {index}>{user.nombre} - {user.email} - {user.perfil}</li>})
+        }
+    </ul>
+    </>
+    
+  )
+}
+
+
